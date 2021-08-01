9b7a3b4663a61c5628580d44488abdbf62c64910
isieun0908 <supiya0908@naver.com>
2021-08-01 19:00:22 +0900

    배제 test

--- dijkstra.py
+++ dijkstra.py
@@ -1,19 +1,41 @@
 import heapq
-def dijkstra(start, station_info, line_info):
+import sys
+def dijkstra(start, station_info, line_info, user_info):
     distance = {}
     distance[start] = 0
     queue = []
     heapq.heappush(queue, (distance[start], start))
 
+    for user in user_info:
+        print(user_info[user])
+    search_station = []
+    user_station_distance = []
+    max_user_station_distance = sys.float_info.max
+
     while queue:
         current_dist, current_name = heapq.heappop(queue)
+
+        print(current_name)
+        # 다른 user 까지의 거리를 모두 구했다면, 그 중 가장 긴 값 구하기
+        if len(user_station_distance) == len(user_info):
+            max_user_station_distance = max(user_station_distance)
+            print("####################")
+        # 현재 station이 user의 station과 같다면, distance추가
+        for user in user_info:
+            if user_info[user] == current_name:
+                user_station_distance.append(current_dist)
+                print("!!!!!!!!!!!!!!!!!!")
+
+        if distance[current_name] < max_user_station_distance or current_dist < max_user_station_distance:
+            print("++")
         if distance[current_name] < current_dist:
             continue
         for next_station in station_info[current_name]['time'].keys():
             if not (next_station in distance):
                 distance[next_station] = 1000000
-            if distance[next_station] > current_dist+station_info[current_name]['time'][next_station]:
-                distance[next_station] = current_dist+station_info[current_name]['time'][next_station]
+            if distance[next_station] > current_dist + station_info[current_name]['time'][next_station]:
+                distance[next_station] = current_dist + station_info[current_name]['time'][next_station]
                 heapq.heappush(queue, (distance[next_station], next_station))
 
+    print(user_station_distance)
     return distance
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import json
+
+import dijkstra
 import preprocessing
 import middle_station
-
+import dijkstra
 if __name__ == '__main__':
     # 'vertices.json & station.json' 파일 입력 및 딕셔너리 line 생성
     line_info = {}  # 딕셔너리 생성 : 호선 정보
@@ -11,4 +13,5 @@ if __name__ == '__main__':
     user_info = {}                    # user 정보 저장할 딕셔너리
     preprocessing.input_data(user_info)
 
-    middle_station.middle_station(station_info, line_info, user_info)
+    dijkstra.dijkstra("금정", station_info, line_info, user_info)
+    #middle_station.middle_station(station_info, line_info, user_info)
