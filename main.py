3e825631b899cd89bf8aef13b638219a5ad676e8
daeni-dang <de021@naver.com>
2021-08-07 14:57:14 +0900

    quick_point 함수(가장 빠른 시간에 만나는 역 찾는 함수) 추가

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import dijkstra
 import preprocessing
 import middle_station
+import quick_point
 import dijkstra
 if __name__ == '__main__':
     # 'vertices.json & station.json' 파일 입력 및 딕셔너리 line 생성
@@ -15,3 +16,4 @@ if __name__ == '__main__':
 
     dijkstra.dijkstra("금정", station_info, line_info, user_info)
     #middle_station.middle_station(station_info, line_info, user_info)
+    quick_point.quick_point(station_info, line_info, user_info)
