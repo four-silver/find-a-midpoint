33c871cd882943782207813cfb807869b8ba0923
daeni-dang <de021@naver.com>
2021-08-12 20:40:51 +0900

    quick_point 함수 각자 거리 출력되게 수정

--- quick_point.py
+++ quick_point.py
@@ -13,7 +13,7 @@ def quick_point(station_info, line_info, user_info):
         for j in range(len(user_info)):
             user_distance.append(dijkstra_result[j][station])
         each_user_distance[station] = user_distance
-    #print(each_user_distance)
+    # print(each_user_distance)
 
     max_distance = {}   # 네 명이 소요되는 시간 중 가장 큰 시간 집어넣음
     for fastest in each_user_distance:
@@ -26,4 +26,9 @@ def quick_point(station_info, line_info, user_info):
             min_distance = i    # max_distancs 중에서 가장 작은 값이 모두 가장 빨리 만나는 지점
     quick_station = min(max_distance, key=max_distance.get)
 
+    print("\n#######################")
     print("가장 빨리 만나는 역:",quick_station)
+    each_station = 0
+    for name in user_info.keys():
+        print(name+" :", each_user_distance[quick_station][each_station])
+        each_station += 1
