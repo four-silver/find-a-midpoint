f2136819295057146e44803462029969169444b9
isieun0908 <supiya0908@naver.com>
2021-08-13 14:46:40 +0900

    quick_point.py 주석

--- quick_point.py
+++ quick_point.py
@@ -1,31 +1,38 @@
 import dijkstra
+import sys
 
 # 동시에 출발했을 때 모든 구성원의 도착 지점이 가장 빠른 지점
 def quick_point(station_info, line_info, user_info):
+    # 탐색할 역 이름 저장
+    search_station = list(station_info.keys())
+
+    # user별 dijkstra 수행
     dijkstra_result = []
-    each_user_distance = {}
     for i in user_info:
-        dijkstra_result.append(dijkstra.dijkstra(user_info[i], station_info, line_info, user_info))
-    for station in station_info:
+        dijkstra_result.append(dijkstra.dijkstra(user_info[i], station_info, user_info, search_station))
+
+    # 각 역별로 user까지 거리 list 생성
+    # ex. '가능': [115, 19, 87, 107]
+    each_user_distance = {}
+    for station in search_station:
         user_distance = []
         for j in range(len(user_info)):
             user_distance.append(dijkstra_result[j][station])
         each_user_distance[station] = user_distance
-    # print(each_user_distance)
 
-    max_distance = {}   # 네 명이 소요되는 시간 중 가장 큰 시간 집어넣음
-    for fastest in each_user_distance:
-        max_distance[fastest] = max(each_user_distance[fastest])
+    # 각 역별로 '소요시간(user의 distance 중 가장 큰 값)' 구하기
+    # ex. '가능': 115
+    max_distance = {}
+    for station in search_station:
+        max_distance[station] = max(each_user_distance[station])        # 소요시간
 
-    min_distance = 10000
-
-    for i in max_distance.values():
-        if min_distance > i:
-            min_distance = i    # max_distancs 중에서 가장 작은 값이 모두 가장 빨리 만나는 지점
+    # 소요시간이 가장 작은 역 구하기
     quick_station = min(max_distance, key=max_distance.get)
 
+    # 출력
     print("\n#######################")
     print("가장 빨리 만나는 역:",quick_station)
+
     each_station = 0
     for name in user_info.keys():
         print(name+" :", each_user_distance[quick_station][each_station])
