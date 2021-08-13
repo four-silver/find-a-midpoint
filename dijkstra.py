7bc98c459de16403c4080b47317b35323f3acf2c
isieun0908 <supiya0908@naver.com>
2021-08-13 20:25:56 +0900

    시은이 오류 수정

--- dijkstra.py
+++ dijkstra.py
@@ -1,5 +1,6 @@
 import heapq
 import sys
+
 def dijkstra(start, station_info, line_info, user_info):
     distance = {}
     distance[start] = 0
@@ -16,18 +17,18 @@ def dijkstra(start, station_info, line_info, user_info):
     while queue:
         current_dist, current_name = heapq.heappop(queue)
 
-        #print(current_name)
+        # print(current_name)
         # 다른 user 까지의 거리를 모두 구했다면, 그 중 가장 긴 값 구하기
         if len(user_station_distance) == len(user_info):
             max_user_station_distance = max(user_station_distance)
-            #print("####################")
+            # print("####################")
         # 현재 station이 user의 station과 같다면, distance추가
         for user in user_info:
             if user_info[user] == current_name:
                 user_station_distance.append(current_dist)
-                #print("!!!!!!!!!!!!!!!!!!")
-        #if distance[current_name] < max_user_station_distance or current_dist < max_user_station_distance:
-            #print("++")
+                # print("!!!!!!!!!!!!!!!!!!")
+        # if distance[current_name] < max_user_station_distance or current_dist < max_user_station_distance:
+        # print("++")
         if distance[current_name] < current_dist:
             continue
         for next_station in station_info[current_name]['time'].keys():
@@ -38,9 +39,9 @@ def dijkstra(start, station_info, line_info, user_info):
                 heapq.heappush(queue, (distance[next_station], next_station))
 
                 if not (current_name in path):
-                    path[current_name]=[current_name]
-                    
-                path[next_station]=[next_station]
+                    path[current_name] = [current_name]
+
+                path[next_station] = [next_station]
                 for x in path[current_name]: path[next_station].append(x)
 
     print(user_station_distance)
