368855de6517593cff850c0849d521e36bb3429b
isieun0908 <supiya0908@naver.com>
2021-09-17 22:34:38 +0900

    dfs 수정

--- dfs.py
+++ dfs.py
@@ -1,42 +1,30 @@
-import preprocessing
+import sys
 
-dfs= []
-dist={}
-path=" "
-n=" "
+dfs = []
+dist = {}
+path = " "
 
-def DFS(current_station, sum,s,cost,station_info):
-    print ('@')
+def DFS(current_station, sum, s, cost, station_info):
     if not current_station in dist:
-        dist[current_station]=9999
+        dist[current_station] = sys.maxsize
 
-    if (dist[current_station]<sum):
+    if dist[current_station] < sum:
         return
     else:
-        dist[current_station]=sum
+        dist[current_station] = sum
 
-    if (sum>cost):
-            return
-
-    if (current_station==n):
-        if(sum<cost):
-            cost=sum
-            path=s
+    if sum > cost:
         return
 
     for next_station in station_info[current_station]['time'].keys():
       if not next_station in dfs:
         dfs.append(next_station)
-        print (next_station)
-        print(s)
-        DFS(next_station,sum+ station_info[current_station]['time'][next_station],s+next_station,cost,station_info)
+        DFS(next_station,sum + station_info[current_station]['time'][next_station],s+next_station,cost,station_info)
         dfs.remove(next_station)
 
-
-def start(start, end, station_info):
-    cost=9999
-    n=end
+def start(start, station_info):
+    cost = sys.maxsize
     dist[start] = cost
     dfs.append(start)
-    DFS(start,0," ",cost, station_info)
-
+    DFS(start, 0, " ", cost, station_info)
+    return dist
