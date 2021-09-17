c26565ff7099a4e7d456050b6d3d19bcde7c4284
kimgaeun21 <kimgaeun21@naver.com>
2021-09-17 22:00:02 +0900

    dfs

--- dfs.py
+++ dfs.py
@@ -5,7 +5,7 @@ dist={}
 path=" "
 n=" "
 
-def DFS(current_station, sum, s,cost):
+def DFS(current_station, sum,s,cost,station_info):
     print ('@')
     if not current_station in dist:
         dist[current_station]=9999
@@ -28,18 +28,15 @@ def DFS(current_station, sum, s,cost):
       if not next_station in dfs:
         dfs.append(next_station)
         print (next_station)
-        DFS(next_station,sum+station_info[current_station]['time'][next_station],next_station,cost)
+        print(s)
+        DFS(next_station,sum+ station_info[current_station]['time'][next_station],s+next_station,cost,station_info)
         dfs.remove(next_station)
 
 
-
-
-def start(start, end):
+def start(start, end, station_info):
     cost=9999
     n=end
     dist[start] = cost
     dfs.append(start)
-    DFS(start,0," ",cost)
+    DFS(start,0," ",cost, station_info)
 
-start("충무로","서울역")
-print (path)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import SPFA
 import preprocessing
 import quick_point
+import dfs
 
 if __name__ == '__main__':
     # 'vertices.json & station.json' 파일 입력 및 딕셔너리 line 생성
@@ -15,3 +16,6 @@ if __name__ == '__main__':
     quick_point.dijkstra_quickpoint(station_info, user_info)
     print("\nSPFA")
     quick_point.SPFA_quickpoint(station_info, user_info)
+
+    dfs.start("충무로", "서울역",station_info)
+    print(dfs.path)
